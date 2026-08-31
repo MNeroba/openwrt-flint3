168e272ef930a99ce67d93ac53b5b37cf7026b44
Maksim Neroba <mneroba@MB14.local>
2026-08-31 22:32:36 +0300

    rtl837x: propagate auxiliary probe failures
    Abort probe and unwind the DSA switch when GPIO-controller or SFP registration fails.
    
    Signed-off-by: Maksim Neroba <mneroba@MB14.local>

--- package/kernel/rtl837x/src/rtl837x_mdio.c
+++ package/kernel/rtl837x/src/rtl837x_mdio.c
@@ -665,6 +665,8 @@ static int rtl837x_sfp_probe(struct rtk_gsw *gsw)
 	gsw->sfp_bus = bus;
 
 	ret = sfp_bus_add_upstream(bus, gsw, &sfp_ops);
+	if (ret)
+		gsw->sfp_bus = NULL;
 	sfp_bus_put(bus);
 
 	return ret;
@@ -875,11 +877,29 @@ static int rtl837x_dsa_probe(struct mdio_device *mdiodev)
 	}
 
 #ifdef CONFIG_GPIOLIB
-	if (of_property_read_bool(np, "gpio-controller")) 
-		rtl837x_gpiochip_init(gsw);
+	if (of_property_read_bool(np, "gpio-controller")) {
+		ret = rtl837x_gpiochip_init(gsw);
+		if (ret) {
+			dev_err_probe(dev, ret,
+				      "failed to register GPIO controller\n");
+			rtl837x_dsa_unregister(gsw);
+			if (master)
+				dev_put(master);
+			devm_kfree(dev, gsw);
+			return ret;
+		}
+	}
 #endif /* CONFIG_GPIOLIB */
 
-	rtl837x_sfp_probe(gsw);
+	ret = rtl837x_sfp_probe(gsw);
+	if (ret) {
+		dev_err_probe(dev, ret, "failed to attach SFP bus\n");
+		rtl837x_dsa_unregister(gsw);
+		if (master)
+			dev_put(master);
+		devm_kfree(dev, gsw);
+		return ret;
+	}
 
 	rtl837x_debug_proc_init(gsw);
 	rtl837x_status_check_work_init(gsw);
